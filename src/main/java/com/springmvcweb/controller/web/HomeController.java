e7b9c7ad1d3a6d01a516f60ee0c848b9955b851b
ThanhTungTrieu <thanhtungcb.2001@gmail.com>
2021-05-04 00:08:52 +0700

    Login and Logout

--- src/main/java/com/springmvcweb/controller/web/HomeController.java
+++ src/main/java/com/springmvcweb/controller/web/HomeController.java
@@ -1,9 +1,15 @@
 package com.springmvcweb.controller.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller (value = "homeControllerOfWeb")
 public class HomeController {
@@ -18,5 +24,25 @@ public class HomeController {
     public ModelAndView springMVCPage() {
         ModelAndView mav = new ModelAndView("springmvc");
         return mav;
+    }
+
+    @RequestMapping(value = "/dang-nhap", method = RequestMethod.GET)
+    public ModelAndView loginPage() {
+        ModelAndView mav = new ModelAndView("login");
+        return mav;
+    }
+
+    @RequestMapping(value = "/thoat", method = RequestMethod.GET)
+    public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return new ModelAndView("redirect:/trang-chu");
+    }
+
+    @RequestMapping(value = "/access-denied", method = RequestMethod.GET)
+    public ModelAndView accessDenied() {
+        return new ModelAndView("redirect:/dang-nhap?access_denied");
     }
 }
