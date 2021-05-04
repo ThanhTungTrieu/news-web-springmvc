69b61fd2eb89330d03d8764086fead00696ab353
ThanhTungTrieu <thanhtungcb.2001@gmail.com>
2021-05-05 00:40:00 +0700

    list controller added

--- src/main/java/com/springmvcweb/controller/admin/HomeController.java
+++ src/main/java/com/springmvcweb/controller/admin/HomeController.java
@@ -1,5 +1,7 @@
 package com.springmvcweb.controller.admin;
 
+import com.springmvcweb.service.INewsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +10,19 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller (value = "homeControllerOfAdmin")
 public class HomeController {
 
+    @Autowired
+    private INewsService newsService;
+
     @RequestMapping(value = "/quan-tri/trang-chu", method = RequestMethod.GET)
     public ModelAndView homepage() {
         ModelAndView mav = new ModelAndView("admin/home");
+        return mav;
+    }
+
+    @RequestMapping(value = "quan-tri/bai-viet/danh-sach", method = RequestMethod.GET)
+    public ModelAndView listNewsPage() {
+        ModelAndView mav = new ModelAndView("admin/news/list");
+        mav.addObject("model", newsService.findAll());
         return mav;
     }
 }
