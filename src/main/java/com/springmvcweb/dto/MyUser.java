6aea33469457738c434e99e8a97e11eb0f9253ef
ThanhTungTrieu <thanhtungcb.2001@gmail.com>
2021-05-04 21:58:54 +0700

    DTO 2nd commit

--- src/main/java/com/springmvcweb/dto/MyUser.java
+++ src/main/java/com/springmvcweb/dto/MyUser.java
@@ -1,4 +1,4 @@
-package com.springmvcweb.DTO;
+package com.springmvcweb.dto;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
