a8036a69147161726d76385566fef4f352a78505
ThanhTungTrieu <thanhtungcb.2001@gmail.com>
2021-05-11 19:46:52 +0700

    dto -> entity and so on

---
+++ src/main/java/com/springmvcweb/converter/NewsConverter.java
@@ -0,0 +1,29 @@
+package com.springmvcweb.converter;
+
+import com.springmvcweb.dto.NewsDTO;
+import com.springmvcweb.entity.NewsEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NewsConverter {
+
+    public NewsDTO toDTO(NewsEntity entity) {
+        NewsDTO result = new NewsDTO();
+        result.setId(entity.getId());
+        result.setTitle(entity.getTitle());
+        result.setThumbnail(entity.getThumbnail());
+        result.setShortDescription(entity.getShortDescription());
+        result.setContent(entity.getContent());
+        result.setCategoryCode(entity.getCategory().getCode());
+        return result;
+    }
+
+    public NewsEntity toEntity(NewsDTO dto) {
+        NewsEntity result = new NewsEntity();
+        result.setTitle(dto.getTitle());
+        result.setThumbnail(dto.getThumbnail());
+        result.setShortDescription(dto.getShortDescription());
+        result.setContent(dto.getContent());
+        return result;
+    }
+}
